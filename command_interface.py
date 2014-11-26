2e48d82fbdeeefa9e3a6836a30030c6c2eedb60f
rzoli <zoltan.raics@raicszoltan.hu>
2014-11-26 05:37:12 +0100

    command protocol updated (not tested)

--- command_interface.py
+++ command_interface.py
@@ -1,7 +1,6 @@
 '''
 This module implements a command/remote function call interface via serial port and tcp/ip socket using the following protocol:
-    SOC<command/function name>EOC<parameters>EOP
-    Parameters:par1,par2,....parn, kwpar1=parA,kwpar2=parB....
+    <command/function name>(<parameter1>,<parameter2>)
 '''
 import os
 import serial
@@ -25,36 +24,26 @@ class ProtocolHandler(object):
     def parse(self):
         while not self.queue_in.empty():
             self.buffer+=self.queue_in.get()
-        soc_index = self.buffer.find('SOC')
-        eoc_index = self.buffer.find('EOC')
-        eop_index = self.buffer.find('EOP')
-        if soc_index == -1 or  eoc_index == -1 or eop_index == -1:
+        eoc_index = self.buffer.find('(')
+        eop_index = self.buffer.find(')')
+        if eoc_index == -1 or eop_index == -1:
             #packet is not ready
             return
-        if soc_index < eoc_index < eop_index:
-            command = self.buffer[soc_index+3:eoc_index]
-            parameters = self.buffer[eoc_index+3:eop_index].split(',')
-            kw_parameters = {}
-            nonkw_parameters = []
-            for parameter in parameters:
-                if '=' in parameter:
-                    chunks = parameter.split('=')
-                    kw_parameters[chunks[0]] = chunks[1]
-                else:
-                    nonkw_parameters.append(parameter)
+        if eoc_index < eop_index:
+            command = self.buffer[:eoc_index]
+            parameters = self.buffer[eoc_index+1:eop_index].split(',')
             parsed_command = {}
             parsed_command['command'] = command
-            parsed_command['args'] = nonkw_parameters
-            parsed_command['kwargs'] = kw_parameters
-            self.buffer=self.buffer.replace(self.buffer[soc_index:eop_index+3],'')
+            parsed_command['args'] = parameters
+            self.buffer=self.buffer.replace(self.buffer[:eop_index+1],'')
             self.parsed_commands.put(parsed_command)
         else:
             raise RuntimeError('This command cannot be parsed: {0}'.format(self.buffer))
         pass
         
-    def send_command(self, command, *args, **kwargs):
+    def send_command(self, command, *args):
         '''
-        args and kwargs must contain only strings or numeric values
+        args shall contain only strings or numeric values
         '''
         parameters = ''
         valid_types=[float,int,str]
@@ -65,15 +54,8 @@ class ProtocolHandler(object):
             if isinstance(arg, str) and any([c in arg for c in reserved_chars]):
                raise RuntimeError('Parameter contains reserved character: {0}'.format(arg)) 
             parameters += '{0},'.format(arg)
-        for k,v in kwargs.items():
-            if not any([isinstance(v, type) for type in valid_types]):
-                raise RuntimeError('{0} parameter is not valid datatype: {1}. Valid datatypes: {2}'.format(v, type(v),valid_types)) 
-            for item in [k,v]:
-                if isinstance(item, str) and any([c in item for c in reserved_chars]):
-                   raise RuntimeError('Parameter contains reserved character: {0}'.format(item)) 
             
-            parameters += '{0}={1},'.format(k,v)
-        message = 'SOC{0}EOC{1}EOP'.format(command, parameters[:-1])
+        message = '{0}({1})'.format(command, parameters[:-1])
         self.queue_out.put(message)
         
 class SerialPortHandler(threading.Thread, ProtocolHandler):
@@ -112,48 +94,41 @@ class TestProtocolHandler(unittest.TestCase):
         par2=1
         par3=123.0
         par4='abc'
-        self.p.send_command('cmd', par1, par2, par3=par3, par4=par4)
-        self.assertEqual(self.p.queue_out.get(),'SOCcmdEOCa,1,par4=abc,par3=123.0EOP')
+        self.p.send_command('cmd', par1, par2, par3, par4)
+        self.assertEqual(self.p.queue_out.get(),'cmd(a,1,123.0,abc)')
         
     def test_02_invalid_datatype(self):
         par1=['a']
         par2=1
         par3=123.0
         par4='abc'
-        self.assertRaises(RuntimeError, self.p.send_command, 'cmd', par1, par2, par3=par3, par4=par4)
+        self.assertRaises(RuntimeError, self.p.send_command, 'cmd', par1, par2, par3, par4)
         
     def test_03_invalid_datatype(self):
         par1='a'
         par2=1
         par3=[123.0]
         par4='abc'
-        self.assertRaises(RuntimeError, self.p.send_command, 'cmd', par1, par2, par3=par3, par4=par4)
+        self.assertRaises(RuntimeError, self.p.send_command, 'cmd', par1, par2, par3, par4)
         
     def test_04_invalid_string(self):
         par1='a,'
         par2=1
         par3=[123.0]
         par4='abc'
-        self.assertRaises(RuntimeError, self.p.send_command, 'cmd', par1, par2, par3=par3, par4=par4)
-        
-    def test_05_invalid_string(self):
-        par1='a'
-        par2=1
-        par3=[123.0]
-        par4='ab=c'
-        self.assertRaises(RuntimeError, self.p.send_command, 'cmd', par1, par2, par3=par3, par4=par4)
+        self.assertRaises(RuntimeError, self.p.send_command, 'cmd', par1, par2, par3, par4)
         
     def test_06_parse(self):
-        for c in 'SOCcmdEOCa,1,par4=abc,par3=123.0EOP':
+        for c in 'cmd(a,1,abc,123.0)':
             self.p.queue_in.put(c)
         self.p.parse()
-        self.assertEqual(self.p.parsed_commands.get(), {'args': ['a', '1'], 'command': 'cmd', 'kwargs': {'par4': 'abc', 'par3': '123.0'}})
+        self.assertEqual(self.p.parsed_commands.get(), {'args': ['a', '1', 'abc', '123.0'], 'command': 'cmd' })
         
     def test_07_parse(self):
-        for c in 'abcSOCcmdEOCa,1,par4=abc,par3=123.0EOPcde':
+        for c in 'cmd(a,1,abc,123.0)cde':
             self.p.queue_in.put(c)
         self.p.parse()
-        self.assertEqual(self.p.parsed_commands.get(), {'args': ['a', '1'], 'command': 'cmd', 'kwargs': {'par4': 'abc', 'par3': '123.0'}})
+        self.assertEqual(self.p.parsed_commands.get(), {'args': ['a', '1', 'abc', '123.0'], 'command': 'cmd'})
         
 class TestSerialPortHandler(unittest.TestCase):
     '''
@@ -165,15 +140,15 @@ class TestSerialPortHandler(unittest.TestCase):
         This test expects that the microcontroller is connected or at least a serial port is connected in loopback mode
         '''
         self.s=SerialPortHandler('/dev/ttyUSB0',115200,0.1,Queue.Queue(),Queue.Queue(),Queue.Queue())
-        self.s.queue_out.put('abcSOCechoEOC12345EOPcde')
-        self.s.queue_out.put('abc12345SOCechoEOC67890EOPcde')
+        self.s.queue_out.put('abc()echo(12345)cde')
+        self.s.queue_out.put('abc12345()echo(67890)cde')
         self.s.start()
         time.sleep(1)
         self.s.terminate=True
         self.s.join()
         self.assertEqual((self.s.parsed_commands.get(timeout=1),self.s.parsed_commands.get(timeout=1)),
-                            ({'args': ['12345'], 'command': 'echo', 'kwargs': {}},
-                            {'args': ['67890'], 'command': 'echo', 'kwargs': {}}))
+                            ({'args': ['12345'], 'command': 'echo'},
+                            {'args': ['67890'], 'command': 'echo'}))
         
 
 if __name__ == "__main__":
