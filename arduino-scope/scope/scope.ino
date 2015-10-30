e32f5d847c2d8f59575598972258a1629a3eade2
rzoli <zoltan.raics@raicszoltan.hu>
2015-10-30 16:35:03 +0100

    adc init (not ready)

--- arduino-scope/scope/scope.ino
+++ arduino-scope/scope/scope.ino
@@ -1,5 +1,15 @@
 #include <SoftwareSerial.h>
-
+/*
+ * TODOS
+ * 
+ * 1) Control pin 8-11 using first 4 bits of a serial command byte
+ * 2) Read analog input. channel selection by using serial port command byte's last four bits. Digital inputs are enabled by selecting channel 0
+ * 
+ *?? ADC interrupt, when to read adc values
+ * 
+ * 
+ * 
+ */
 byte portd_val;
 
 #define COMPARE 75//FCPU is 14.7456MHz, comp=FCPU/(f*prescale)+1
@@ -7,12 +17,18 @@ byte portd_val;
 
 #define PORT_PIN(port,pin,pos) ((port&(1<<pin))>>pin)<<pos
 
+#define ADC5  0
+#define ADC4  1
+#define ADC3  4
+#define ADC2  5
+#define ADC1  6
+#define ADC0  7
+
+
 ISR(TIMER3_COMPA_vect) {
    PORTB |=(1<<6);
-   
    TCNT3L=0;
    TCNT3H=0;
-   
    portd_val=0;
    portd_val|=PORT_PIN(PIND,0,3);
    portd_val|=PORT_PIN(PIND,1,2);
@@ -26,6 +42,12 @@ ISR(TIMER3_COMPA_vect) {
    PORTB &=~(1<<6);
 }
 
+void init_adc(void)
+{
+   ADMUX=1<<6|1<<5|ADC0;//vcc as vref, left adjusted result, will use it in 8 bit mode, AD0 channel on board
+   ADCSRA=1<<7|1<<6|1<<5;
+
+}
 
 void setup() {
   cli();
@@ -50,6 +72,7 @@ void setup() {
   TIMSK3|= 1<<1;
   TCNT3L=0;
   TCNT3H=0;
+  init_adc();
   sei();
 
 }
